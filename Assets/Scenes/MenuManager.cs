95e40e88b963aee042f1a50d56c7ec4e3c068510
Reni-Rena <81166304+Reni-Rena@users.noreply.github.com>
2024-04-10 14:08:14 +0200

    2.0

---
+++ Assets/Scenes/MenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("SimplePoly City - Low Poly Assets_Demo Scene");
+    }
+    public void QuiteGame()
+    {
+        Application.Quit();
+    }
+}
